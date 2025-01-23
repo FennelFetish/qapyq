b5b30181d6aaf5d14bdbe742d9ca01049ebd7e3a
Fennel <fennel@alchemists.ch>
2025-01-23 14:05:05 +0100

    Batch Mask: Load input layers from file/alpha channel

--- batch/batch_mask.py
+++ batch/batch_mask.py
@@ -1,26 +1,40 @@
-from PySide6 import QtWidgets
-from PySide6.QtCore import Qt, Slot, QSignalBlocker
-from PySide6.QtGui import QImageReader
 import os
+from enum import Enum
+from typing import Callable
+from PySide6 import QtWidgets
+from PySide6.QtCore import QSignalBlocker, Qt, Slot
+from PySide6.QtGui import QImageReader
 import cv2 as cv
 import numpy as np
 from config import Config
+from infer import Inference
 from lib import qtlib
+from lib.filelist import MASK_EXTENSIONS
 from lib.mask_macro import MaskingMacro
 from lib.mask_macro_vis import MacroVisualization
-from infer import Inference
-from .batch_task import BatchTask, BatchSignalHandler, BatchUtil
 import ui.export_settings as export
+from .batch_task import BatchSignalHandler, BatchTask, BatchUtil
 
 
 # TODO: Store detections in json (or separate batch detect?)
 
-MASK_SAVE_MODE_FILE  = "file"
-MASK_SAVE_MODE_ALPHA = "alpha"
+class MaskSrcMode(Enum):
+    NewBlack       = "new-black"
+    NewWhite       = "new-white"
+    FileFirstLayer = "file-1"
+    File4Layers    = "file-4"
+    Alpha          = "alpha"
+
+SRC_MODE_NEW_LAYERS = (MaskSrcMode.NewBlack, MaskSrcMode.NewWhite)
+
+class MaskDestMode(Enum):
+    File  = "file"
+    Alpha = "alpha"
 
 
 class BatchMask(QtWidgets.QWidget):
-    EXPORT_PRESET_KEY = "batch-mask"
+    EXPORT_PRESET_KEY_SRC  = "batch-mask-input"
+    EXPORT_PRESET_KEY_DEST = "batch-mask"
 
     def __init__(self, tab, logSlot, progressBar, statusBar):
         super().__init__()
@@ -32,10 +46,15 @@ class BatchMask(QtWidgets.QWidget):
         self.parser = export.ExportVariableParser()
         self.parser.setup(self.tab.filelist.getCurrentFile(), None)
 
-        config = Config.exportPresets.get(self.EXPORT_PRESET_KEY, {})
-        self.pathSettings = export.PathSettings(self.parser, showInfo=False, showSkip=True)
-        self.pathSettings.pathTemplate   = config.get("path_template", "{{path}}-masklabel")
-        self.pathSettings.overwriteFiles = config.get("overwrite", True)
+        srcConfig = Config.exportPresets.get(self.EXPORT_PRESET_KEY_SRC, {})
+        self.srcPathSettings = export.PathSettings(self.parser, showInfo=False)
+        self.srcPathSettings.setAsInput()
+        self.srcPathSettings.pathTemplate = srcConfig.get("path_template", "{{path}}-masklabel")
+        
+        destConfig = Config.exportPresets.get(self.EXPORT_PRESET_KEY_DEST, {})
+        self.destPathSettings = export.PathSettings(self.parser, showInfo=False, showSkip=True)
+        self.destPathSettings.pathTemplate   = destConfig.get("path_template", "{{path}}-masklabel")
+        self.destPathSettings.overwriteFiles = destConfig.get("overwrite", True)
 
         self._task = None
         self._taskSignalHandler = None
@@ -50,11 +69,13 @@ class BatchMask(QtWidgets.QWidget):
         layout.setRowStretch(2, 0)
 
         layout.addWidget(self._buildMacroSettings(), 0, 0)
-        layout.addWidget(self._buildDestinationSettings(), 1, 0)
+        layout.addWidget(self._buildInputSettings(), 1, 0)
+        layout.addWidget(self._buildDestinationSettings(), 2, 0)
+        self._onSrcModeChanged(self.cboSrcType.currentIndex())
 
         self.btnStart = QtWidgets.QPushButton("Start Batch Mask")
         self.btnStart.clicked.connect(self.startStop)
-        layout.addWidget(self.btnStart, 2, 0)
+        layout.addWidget(self.btnStart, 3, 0)
 
         self.setLayout(layout)
 
@@ -82,35 +103,69 @@ class BatchMask(QtWidgets.QWidget):
         groupBox.setLayout(layout)
         return groupBox
 
-    def _buildDestinationSettings(self):
+    def _buildInputSettings(self):
         layout = QtWidgets.QGridLayout()
         layout.setColumnStretch(0, 0)
         layout.setColumnStretch(1, 0)
         layout.setColumnStretch(2, 0)
         layout.setColumnStretch(3, 1)
-        layout.setColumnMinimumWidth(2, 20)
-        layout.setRowStretch(0, 0)
-        layout.setRowStretch(1, 0)
-        layout.setRowStretch(2, 0)
+        layout.setColumnMinimumWidth(0, 70)
+        layout.setColumnMinimumWidth(1, 220)
+        layout.setColumnMinimumWidth(2, 8)
+
+        row = 0
+        self.cboSrcType = QtWidgets.QComboBox()
+        self.cboSrcType.addItem("Create New (1 Layer)", MaskSrcMode.NewBlack)
+        #self.cboSrcType.addItem("Create New (1 White Layer)", MaskSrcMode.NewWhite)
+        self.cboSrcType.addItem("Separate Image (First Layer)", MaskSrcMode.FileFirstLayer)
+        self.cboSrcType.addItem("Separate Image (4 Layers)", MaskSrcMode.File4Layers)
+        self.cboSrcType.addItem("Alpha Channel (1 Layer)", MaskSrcMode.Alpha)
+        self.cboSrcType.currentIndexChanged.connect(self._onSrcModeChanged)
+        layout.addWidget(QtWidgets.QLabel("Load from:"), row, 0, Qt.AlignmentFlag.AlignTop)
+        layout.addWidget(self.cboSrcType, row, 1)
+
+        self.chkSkipNoInput = QtWidgets.QCheckBox("Skip processing if input mask is missing")
+        layout.addWidget(self.chkSkipNoInput, row, 3)
+
+        row += 1
+        self.srcPathSettings.layout().setColumnMinimumWidth(0, 70)
+        layout.addWidget(self.srcPathSettings, row, 0, 1, 4)
+
+        groupBox = QtWidgets.QGroupBox("Input Layers")
+        groupBox.setLayout(layout)
+        return groupBox
+
+    def _buildDestinationSettings(self):
+        layout = QtWidgets.QGridLayout()
+        layout.setColumnStretch(0, 0)
+        layout.setColumnStretch(1, 0)
+        layout.setColumnStretch(2, 0)
+        layout.setColumnStretch(3, 0)
+        layout.setColumnStretch(4, 0)
+        layout.setColumnStretch(5, 1)
+        layout.setColumnMinimumWidth(0, 70)
+        layout.setColumnMinimumWidth(1, 220)
+        layout.setColumnMinimumWidth(2, 8)
 
         row = 0
         self.cboDestType = QtWidgets.QComboBox()
-        self.cboDestType.addItem("Separate Image  (Max 4 Layers)", MASK_SAVE_MODE_FILE)
-        self.cboDestType.addItem("Alpha Channel  (Max 1 Layer)", MASK_SAVE_MODE_ALPHA)
-        layout.addWidget(QtWidgets.QLabel("Save in:"), row, 0, Qt.AlignmentFlag.AlignTop)
+        self.cboDestType.addItem("Separate Image  (Max 4 Layers)", MaskDestMode.File)
+        self.cboDestType.addItem("Alpha Channel  (Max 1 Layer)", MaskDestMode.Alpha)
+        layout.addWidget(QtWidgets.QLabel("Save in:"), row, 0)
         layout.addWidget(self.cboDestType, row, 1)
 
-        layout.addWidget(self.pathSettings, row, 3, 3, 1)
-
-        row += 1
         self.lblFormat = QtWidgets.QLabel("Format:")
-        layout.addWidget(self.lblFormat, row, 0, Qt.AlignmentFlag.AlignTop)
+        layout.addWidget(self.lblFormat, row, 3)
 
         self.cboFormat = QtWidgets.QComboBox()
         self.cboFormat.addItems(["PNG", "WEBP"])
         self.cboFormat.currentTextChanged.connect(self._onExtensionChanged)
         self._onExtensionChanged(self.cboFormat.currentText())
-        layout.addWidget(self.cboFormat, row, 1)
+        layout.addWidget(self.cboFormat, row, 4)
+
+        row += 1
+        self.destPathSettings.layout().setColumnMinimumWidth(0, 70)
+        layout.addWidget(self.destPathSettings, row, 0, 1, 6)
 
         groupBox = QtWidgets.QGroupBox("Destination")
         groupBox.setLayout(layout)
@@ -120,13 +175,22 @@ class BatchMask(QtWidgets.QWidget):
     def onFileChanged(self, file: str):
         self.parser.setup(file)
         self.parser.setImageDimension(self.tab.imgview.image.pixmap())
-        self.pathSettings.updatePreview()
+        self.destPathSettings.updatePreview()
 
 
     @Slot()
+    def _onSrcModeChanged(self, index: int):
+        mode = self.cboSrcType.itemData(index)
+        pathEnabled = (mode in (MaskSrcMode.FileFirstLayer, MaskSrcMode.File4Layers))
+        self.srcPathSettings.setEnabled(pathEnabled)
+
+        skipEnabled = (mode not in SRC_MODE_NEW_LAYERS)
+        self.chkSkipNoInput.setEnabled(skipEnabled)
+
+    @Slot()
     def _onExtensionChanged(self, ext: str):
-        self.pathSettings.extension = ext
-        self.pathSettings.updatePreview()
+        self.destPathSettings.extension = ext
+        self.destPathSettings.updatePreview()
 
 
     @Slot()
@@ -150,17 +214,36 @@ class BatchMask(QtWidgets.QWidget):
     def _confirmStart(self) -> bool:
         ops = [f"Generate masks using the '{self.cboMacro.currentText()}' macro"]
 
-        if self.pathSettings.skipExistingFiles:
+        if self.destPathSettings.skipExistingFiles:
             ops.append("Skip the mask generation if the target file already exists")
 
-        if self.cboDestType.currentData() == MASK_SAVE_MODE_FILE:
+        srcMode: MaskSrcMode = self.cboSrcType.currentData()
+        match srcMode:
+            case MaskSrcMode.NewBlack:
+                ops.append("Start the mask with 1 new black layer")
+            case MaskSrcMode.NewWhite:
+                ops.append("Start the mask with 1 new white layer")
+            case MaskSrcMode.FileFirstLayer:
+                ops.append("Start the mask with the first layer of an existing mask")
+            case MaskSrcMode.File4Layers:
+                ops.append("Start the mask with 4 layers of an existing mask")
+            case MaskSrcMode.Alpha:
+                ops.append("Start the mask with the alpha channel from the image")
+
+        if srcMode not in SRC_MODE_NEW_LAYERS:
+            if self.chkSkipNoInput.isChecked():
+                ops.append("Skip processing if the input mask doesn't exist")
+            else:
+                ops.append("Start the mask with empty layers if the input mask doesn't exist")
+
+        if self.cboDestType.currentData() == MaskDestMode.File:
             ops.append("Store the mask as a separate image")
         else:
             ops.append("Store the mask as the alpha channel in the image")
 
-        if self.pathSettings.overwriteFiles:
+        if self.destPathSettings.overwriteFiles:
             ops.append( qtlib.htmlRed("Overwrite existing images!") )
-        elif self.pathSettings.skipExistingFiles:
+        elif self.destPathSettings.skipExistingFiles:
             ops.append("Save images using new filenames")
         else:
             ops.append("Save images using new filenames with an increasing counter")
@@ -169,9 +252,13 @@ class BatchMask(QtWidgets.QWidget):
 
 
     def saveExportPreset(self):
-        Config.exportPresets[self.EXPORT_PRESET_KEY] = {
-            "path_template": self.pathSettings.pathTemplate,
-            "overwrite": self.pathSettings.overwriteFiles
+        Config.exportPresets[self.EXPORT_PRESET_KEY_SRC] = {
+            "path_template": self.srcPathSettings.pathTemplate
+        }
+
+        Config.exportPresets[self.EXPORT_PRESET_KEY_DEST] = {
+            "path_template": self.destPathSettings.pathTemplate,
+            "overwrite": self.destPathSettings.overwriteFiles
         }
 
     @Slot()
@@ -189,7 +276,23 @@ class BatchMask(QtWidgets.QWidget):
 
         macroPath = self.cboMacro.currentData()
         saveMode = self.cboDestType.currentData()
-        self._task = BatchMaskTask(self.log, self.tab.filelist, macroPath, saveMode, self.pathSettings)
+        self._task = BatchMaskTask(self.log, self.tab.filelist, macroPath, saveMode, self.destPathSettings)
+
+        skipNonExistingSource = self.chkSkipNoInput.isChecked()
+        srcMode: MaskSrcMode = self.cboSrcType.currentData()
+        match srcMode:
+            case MaskSrcMode.NewBlack:
+                self._task.maskSource = newBlackMaskSource
+            case MaskSrcMode.NewWhite:
+                self._task.maskSource = newWhiteMaskSource
+            case MaskSrcMode.FileFirstLayer:
+                self._task.maskSource = createFileMaskSource(self.srcPathSettings.pathTemplate, 1, skipNonExistingSource)
+            case MaskSrcMode.File4Layers:
+                self._task.maskSource = createFileMaskSource(self.srcPathSettings.pathTemplate, 4, skipNonExistingSource)
+            case MaskSrcMode.Alpha:
+                self._task.maskSource = createAlphaMaskSource(skipNonExistingSource)
+            case _:
+                raise ValueError("Invalid mask source mode")
 
         self._taskSignalHandler = BatchSignalHandler(self.statusBar, self.progressBar, self._task)
         self._taskSignalHandler.finished.connect(self.taskDone)
@@ -202,16 +305,79 @@ class BatchMask(QtWidgets.QWidget):
 
 
 
+def newBlackMaskSource(imgPath: str, w: int, h: int) -> list[np.ndarray]:
+    return [ np.zeros((h, w), dtype=np.uint8) ]
+
+def newWhiteMaskSource(imgPath: str, w: int, h: int) -> list[np.ndarray]:
+    return [ np.full((h, w), 255, dtype=np.uint8) ]
+
+
+def createFileMaskSource(pathTemplate: str, numLayers: int, skipNonExisting: bool):
+    parser = export.ExportVariableParser()
+
+    def loadMask(path: str, w: int, h: int) -> list[np.ndarray]:
+        maskMat = cv.imread(path, cv.IMREAD_UNCHANGED)
+        maskH, maskW = maskMat.shape[:2]
+        if maskW != w or maskH != h:
+            raise ValueError("Size of loaded mask does not match image size")
+
+        layers = list(cv.split(maskMat))
+        layers[:3] = layers[2::-1] # Convert BGR(A) -> RGB(A)
+
+        # Ensure minimum number of layers
+        while len(layers) < numLayers:
+            layers.append( np.zeros((h, w), dtype=np.uint8) )
+
+        # Ensure maximum number of layers
+        return layers[:numLayers]
+
+    def fileMaskSource(imgPath: str, w: int, h: int) -> list[np.ndarray]:
+        parser.setup(imgPath)
+        parser.width  = w
+        parser.height = h
+
+        pathNoExt = parser.parsePath(pathTemplate, "", True) # No extension, but with '.' at the end
+        for path in (f"{pathNoExt}{ext}" for ext in MASK_EXTENSIONS):
+            if os.path.exists(path):
+                return loadMask(path, w, h)
+
+        if skipNonExisting:
+            raise MaskSkipException()
+        return [ np.zeros((h, w), dtype=np.uint8) for _ in range(numLayers) ]
+
+    return fileMaskSource
+
+
+def createAlphaMaskSource(skipNonExisting: bool):
+    def alphaMaskSource(imgPath: str, w: int, h: int) -> list[np.ndarray]:
+        imgMat = cv.imread(imgPath, cv.IMREAD_UNCHANGED)
+        channels = imgMat.shape[2] if len(imgMat.shape) > 2 else 1
+        if channels >= 4:
+            return [ np.ascontiguousarray(imgMat[..., 3].copy()) ]
+
+        if skipNonExisting:
+            raise MaskSkipException()
+        return [ np.zeros((h, w), dtype=np.uint8) ]
+    
+    return alphaMaskSource
+
+
+
+class MaskSkipException(Exception): pass
+
 class BatchMaskTask(BatchTask):
-    def __init__(self, log, filelist, macroPath: str, saveMode: str, pathSettings: export.PathSettings):
+    def __init__(self, log, filelist, macroPath: str, saveMode: MaskDestMode, destPathSettings: export.PathSettings):
         super().__init__("mask", log, filelist)
         self.macroPath      = macroPath
-        self.saveMode       = saveMode # file / alpha
+        self.saveMode       = saveMode
 
-        self.pathTemplate   = pathSettings.pathTemplate
-        self.extension      = pathSettings.extension
-        self.overwriteFiles = pathSettings.overwriteFiles
-        self.skipExistingFiles = pathSettings.skipExistingFiles
+        self.pathTemplate   = destPathSettings.pathTemplate
+        self.extension      = destPathSettings.extension
+        self.overwriteFiles = destPathSettings.overwriteFiles
+        self.skipExistingFiles = destPathSettings.skipExistingFiles
+
+        self.maskSource: Callable = None
+        self.maskProcessFunc: Callable = None
 
 
     def runPrepare(self):
@@ -219,16 +385,17 @@ class BatchMaskTask(BatchTask):
         self.macro = MaskingMacro()
         self.macro.loadFrom(self.macroPath)
 
-    def runProcessFile(self, imgFile: str) -> str:
+        match self.saveMode:
+            case MaskDestMode.File:  self.maskProcessFunc = self.processAsSeparateFile
+            case MaskDestMode.Alpha: self.maskProcessFunc = self.processAsAlpha
+            case _: raise ValueError("Invalid destination mode")
+
+    def runProcessFile(self, imgFile: str) -> str | None:
         self.parser.setup(imgFile)
 
-        if self.saveMode == MASK_SAVE_MODE_FILE:
-            path, layers = self.processAsSeparateFile(imgFile)
-        else:
-            path, layers = self.processAsAlpha(imgFile)
-
-        if layers is None:
-            self.log(f"Existing file skipped: {path}")
+        try:
+            path, layers = self.maskProcessFunc(imgFile)
+        except MaskSkipException:
             return None
 
         # BGRA, shape: (h, w, channels)
@@ -238,26 +405,25 @@ class BatchMaskTask(BatchTask):
         export.saveImage(path, combined, self.log)
         return path
 
-    def getPath(self, w: int, h: int) -> tuple[str, bool]:
+    def checkDestinationPath(self, w: int, h: int) -> str:
         self.parser.width = w
         self.parser.height = h
 
         noCounter = self.overwriteFiles or self.skipExistingFiles
         path = self.parser.parsePath(self.pathTemplate, self.extension, noCounter)
-        skip = self.skipExistingFiles and os.path.exists(path)
-        return path, skip
+        if self.skipExistingFiles and os.path.exists(path):
+            raise MaskSkipException()
+        return path
 
 
-    def processAsSeparateFile(self, imgFile: str) -> tuple[str, list[np.ndarray] | None]:
+    def processAsSeparateFile(self, imgFile: str) -> tuple[str, list[np.ndarray]]:
         imgReader = QImageReader(imgFile)
         imgSize = imgReader.size()
         w, h = imgSize.width(), imgSize.height()
 
-        path, skip = self.getPath(w, h)
-        if skip:
-            return path, None
+        path = self.checkDestinationPath(w, h)
 
-        layers = [ np.zeros((h, w), dtype=np.uint8) ]
+        layers = self.maskSource(imgFile, w, h)
         layers, layerChanged = self.macro.run(imgFile, layers)
         layers = layers[:4]
 
@@ -270,13 +436,14 @@ class BatchMaskTask(BatchTask):
         return path, layers
 
 
-    def processAsAlpha(self, imgFile: str) -> tuple[str, list[np.ndarray] | None]:
+    def processAsAlpha(self, imgFile: str) -> tuple[str, list[np.ndarray]]:
         mat = cv.imread(imgFile, cv.IMREAD_UNCHANGED)
         h, w = mat.shape[:2]
 
-        path, skip = self.getPath(w, h)
-        if skip:
-            return path, None
+        path = self.checkDestinationPath(w, h)
+
+        layers = self.maskSource(imgFile, w, h)
+        layers, layerChanged = self.macro.run(imgFile, layers)
 
         channels = mat.shape[2] if len(mat.shape) > 2 else 1
         if channels == 1:
@@ -284,7 +451,5 @@ class BatchMaskTask(BatchTask):
         else:
             imgChannels = list(cv.split(mat))[:3]
 
-        layers = [ np.zeros((h, w), dtype=np.uint8) ]
-        layers, layerChanged = self.macro.run(imgFile, layers)
         imgChannels.append(layers[0])
         return path, imgChannels
