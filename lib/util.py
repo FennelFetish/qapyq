d7c8fbc86424035fd3c7aca8ef38f2841ad1c389
Fennel <fennel@alchemists.ch>
2025-03-16 14:39:49 +0100

    Tag Stats: Added text field for separators (#13). On reload, captions are split by all listed characters. Includes line separator by default (\n). Allows space character to split captions into words.

--- lib/util.py
+++ lib/util.py
@@ -47,3 +47,37 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
+
+
+class CaptionSplitter:
+    def __init__(self, chars: str = ",.:;\n", strings: list[str] = []):
+        self.sepChars = chars.replace("\\n", "\n")
+        self.sepStrings = strings
+
+        if self.sepChars:
+            self.sep = self.sepChars[0]
+            self.trans = str.maketrans({
+                char: self.sep for char in self.sepChars[1:]
+            })
+        else:
+            self.sep = ""
+            self.trans = None
+
+    def split(self, caption: str) -> list[str]:
+        if self.trans is None:
+            capSplit = [caption]
+        else:
+            capSplit = caption.translate(self.trans).split(self.sep)
+
+        for sepString in self.sepStrings:
+            capSplit = [
+                splitPart
+                for cap in capSplit
+                for splitPart in cap.split(sepString)
+            ]
+
+        return [cap for c in capSplit if (cap := c.strip())]
+
+    # def splitReturnSeparators(self, caption: str) -> list[tuple[str, str]]:
+    #     raise NotImplementedError
